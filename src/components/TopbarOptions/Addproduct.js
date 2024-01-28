08d2b46095808bca2814a9670316cd6284d234ff
Bhanu Negi <bhanunegi420@gmail.com>
2024-01-28 23:26:06 +0530

    color picker added along with a bg image and product detail state

--- src/components/TopbarOptions/Addproduct.js
+++ src/components/TopbarOptions/Addproduct.js
@@ -7,19 +7,47 @@ const Addproduct = (props) => {
     const [color, setcolor] = useState('#000000');
     const [allcolors, setallcolors] = useState([]);
 
+    const [currColor, setcurrColor] = useState('#000000');
+
+
+    const [productdetail, setproductdetail] = useState({
+        "productname" : "",
+        "productprice" : 0,
+        "productdescription" : "",
+        "productsize" : [],
+        "productcolor" : [],
+        "producttag" : "",
+    })
+
     const handleClick = () => {
-        // setcolor(color);
-        allcolors.push(color);
-        setallcolors(allcolors);
-        console.log(allcolors);
+        const temp = allcolors;
+        temp.push(color);
+        setallcolors(temp);
+        // console.log(allcolors);
+        setcurrColor(color);
     }
 
     useEffect(() => {
-    
-    }, [allcolors])
+
+    }, [currColor]);
 
     return (
-        <div className="container">
+        <div className="container" style={{ position: 'relative', minHeight: '100vh' }}>
+            <div
+                className="background-image"
+                style={{
+                    backgroundImage: `url(https://firebasestorage.googleapis.com/v0/b/immazon-83e9e.appspot.com/o/login-removebg-preview.png?alt=media&token=c124c472-351b-44e2-b236-44c7954c6c95)`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center center',
+                    position: 'center',
+                    top: 200,
+                    left: 300,
+                    width: '50%',
+                    height: '50%',
+                    opacity: 0.3, // Set the desired opacity value (0 to 1)
+                }}
+            />
             <Sidebar />
             <form className="row g-3">
                 <div className="col-md-6">
@@ -56,20 +84,20 @@ const Addproduct = (props) => {
                         <label className="form-check-label" htmlFor="inlineCheckbox2">S</label>
                     </div>
                     <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2" />
-                        <label className="form-check-label" htmlFor="inlineCheckbox2">M</label>
+                        <input className="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3" />
+                        <label className="form-check-label" htmlFor="inlineCheckbox3">M</label>
                     </div>
                     <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2" />
-                        <label className="form-check-label" htmlFor="inlineCheckbox2">L</label>
+                        <input className="form-check-input" type="checkbox" id="inlineCheckbox4" value="option4" />
+                        <label className="form-check-label" htmlFor="inlineCheckbox4">L</label>
                     </div>
                     <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2" />
-                        <label className="form-check-label" htmlFor="inlineCheckbox2">XL</label>
+                        <input className="form-check-input" type="checkbox" id="inlineCheckbox5" value="option5" />
+                        <label className="form-check-label" htmlFor="inlineCheckbox5">XL</label>
                     </div>
                     <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2" />
-                        <label className="form-check-label" htmlFor="inlineCheckbox2">XXL</label>
+                        <input className="form-check-input" type="checkbox" id="inlineCheckbox6" value="option6" />
+                        <label className="form-check-label" htmlFor="inlineCheckbox6">XXL</label>
                     </div>
 
 
@@ -86,8 +114,8 @@ const Addproduct = (props) => {
                 <div className="container my-2">
                     <label htmlFor="productcolor">Product Color : </label>
                     {
-                        allcolors && allcolors.map((clr)=>{
-                            return <Colorcirle key={clr} color={clr}/>
+                        allcolors && allcolors.map((clr) => {
+                            return <Colorcirle key={clr} color={clr} />
                         })
                     }
                 </div>
